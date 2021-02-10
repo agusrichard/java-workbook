398591dc5263c0061a8b172739e39d54d03370be
Agus Richard <agus.richard21@gmail.com>
2021-02-10 13:21:27 +0700

    What did i learn?

---
+++ algorithms-data-structures/Experiment.java
@@ -0,0 +1,10 @@
+public class Experiment {
+  public static void main(String[] args) {
+    double sekarAge = 21.2;
+    int myAge = (int) sekarAge;
+    System.out.println(myAge);
+
+    String name = "Sekardayu Hana Pradiani";
+    System.out.println(name.length());
+  }
+}
