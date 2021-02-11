dec2b966b1db24a684de94f9d2e3e2302fc38cd3
agusrichard <agus.richard21@gmail.com>
2021-02-11 21:18:48 +0700

    Restructure

--- algorithms-data-structures/Experiment.java
+++
@@ -1,9 +0,0 @@
-public class Experiment {
-  public static void main(String[] args) {
-    String[] names = {"Agus", "Richard", "Lubis"};
-    System.out.println(names);
-
-    int[] nums = {1, 2, 3, 4, 5};
-    System.out.println(nums);
-  }
-}
