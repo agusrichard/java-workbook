551feb48ff4dae705ed89e3d6e2c3231f352b74e
agusrichard <agus.richard21@gmail.com>
2021-02-18 16:33:39 +0700

    Learning how java package works

--- review/oop/Main.java
+++ review/oop/Main.java
@@ -1,3 +1,5 @@
+package oop;
+
 public class Main {
     private int x, y;
 
---
+++ review/pkg/Main.java
@@ -0,0 +1,8 @@
+package pkg;
+
+public class Main {
+  public static void main(String[] args) {
+    Person sekar = new Person("Sekardayu Hana Pradiani", 23);
+    System.out.println(sekar.getName());
+  }
+}
