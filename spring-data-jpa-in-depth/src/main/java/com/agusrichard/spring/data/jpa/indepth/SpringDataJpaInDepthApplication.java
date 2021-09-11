980586f82e200f818db492c1d61235fea36a6ff8
agusrichard <agus.richard21@gmail.com>
2021-09-11 08:23:24 +0700

    Initialize learning Spring Data JPA in depth

---
+++ spring-data-jpa-in-depth/src/main/java/com/agusrichard/spring/data/jpa/indepth/SpringDataJpaInDepthApplication.java
@@ -0,0 +1,13 @@
+package com.agusrichard.spring.data.jpa.indepth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataJpaInDepthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDataJpaInDepthApplication.class, args);
+	}
+
+}
---
+++ spring-data-jpa-in-depth/src/test/java/com/agusrichard/spring/data/jpa/indepth/SpringDataJpaInDepthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.agusrichard.spring.data.jpa.indepth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDataJpaInDepthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
