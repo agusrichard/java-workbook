4ece6564419393a1f9dd6404ec868c6215d2d536
agusrichard <agus.richard21@gmail.com>
2021-11-19 20:03:06 +0700

    Start to learn Spring data JPA

---
+++ sprint-data-jpa-practice/src/main/java/com/agusrichard/sprintdatajpapractice/SprintDataJpaPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.agusrichard.sprintdatajpapractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprintDataJpaPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SprintDataJpaPracticeApplication.class, args);
+	}
+
+}
---
+++ sprint-data-jpa-practice/src/test/java/com/agusrichard/sprintdatajpapractice/SprintDataJpaPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.agusrichard.sprintdatajpapractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SprintDataJpaPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
